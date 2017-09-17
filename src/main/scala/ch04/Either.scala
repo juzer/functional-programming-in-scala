286506a2c758dfdc6b7dc66ae671a455f38f82e4
Piotr Nizio <piotr.nizio@poczta.onet.pl>
2017-09-17 17:47:16 +0200

    Implemented all functions on Either

--- src/main/scala/ch04/Either.scala
+++ src/main/scala/ch04/Either.scala
@@ -6,6 +6,23 @@ sealed trait Either[+E, +A] {
     case Right(a) => Right(f(a))
     case Left(e) => Left(e)
   }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => f(a)
+    case Left(e) => Left(e)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => Right(a)
+    case Left(_) => b
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+    for {
+      a <- this;
+      b1 <- b
+    } yield f(a, b1)
+  }
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
@@ -18,4 +35,11 @@ object Either {
       Left("mean of empty list!")
     else
       Right(xs.sum / xs.length)
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+    case Nil => Right(Nil)
+    case x :: xs => f(x).map2(traverse(xs)(f))(_ :: _)
+  }
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = traverse(es)(x => x)
 }
--- src/test/scala/ch04/EitherTest.scala
+++ src/test/scala/ch04/EitherTest.scala
@@ -12,7 +12,39 @@ class EitherTest extends FunSuite with Matchers {
     assert(Left("this is an error").map("error") == Left("this is an error"))
   }
 
-  test("") {
-    assert(true)
+  test("should flat map successfully") {
+    assert(Right(7).flatMap(a => Right(a * 7)) == Right(49))
+  }
+
+  test("should flat map error to the same error") {
+    assert(Left("this is an error").flatMap(e => Left(e)) == Left("this is an error"))
+  }
+
+  test("should get original value") {
+    assert(Right(43).orElse(Right(-9)) == Right(43))
+  }
+
+  test("should get alternative value") {
+    assert(Left(43).orElse(Right(-9)) == Right(-9))
+  }
+
+  test("should map2 successfully") {
+    assert(Right(2).map2(Right(3))(_ + _) == Right(5))
+  }
+
+  test("should return left when object is left") {
+    assert(Left("err").map2(Right(3))((a, _) => a) == Left("err"))
+  }
+
+  test("should return left when argument is left") {
+    assert(Right(5).map2(Left("err"))((a, _) => a) == Left("err"))
+  }
+
+  test("should traverse a list") {
+    assert(Either.traverse(List(1, 2, 3))(a => Right(a)) == Right(List(1, 2, 3)))
+  }
+
+  test("should sequence a list") {
+    assert(Either.sequence(List(Right(1), Right(2), Right(3))) == Right(List(1, 2, 3)))
   }
 }
