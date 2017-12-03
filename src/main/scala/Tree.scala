c62bc0f21e9baf891f73275f96912b0c780a5e0a
Piotr Nizio <piotr.nizio@poczta.onet.pl>
2017-12-03 18:11:50 +0100

    Implemented swapping the tree

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,7 +1,11 @@
+import scala.io.StdIn
+
 object Solution {
 
   sealed trait Tree[+A]
+
   case class Leaf[A](value: A) extends Tree[A]
+
   case class Branch[A](value: A, left: Option[Tree[A]], right: Option[Tree[A]]) extends Tree[A]
 
   def buildNode(n: Int, maxN: Int, nodes: Array[String]): Tree[Int] = {
@@ -33,10 +37,57 @@ object Solution {
     case _ => {}
   }
 
+  def swap(tree: Option[Tree[Int]], lvl: Int, swpAt: Int): Option[Tree[Int]] = tree match {
+    case Some(Branch(v: Int, l, r)) => {
+      if (v == -1) {
+        return tree
+      }
+      if (lvl == swpAt) {
+        Some(Branch(v, r, l))
+      } else {
+        Some(Branch(v, swap(l, lvl + 1, swpAt), swap(r, lvl + 1, swpAt)))
+      }
+    }
+    case Some(Leaf(v)) => {
+      Some(Leaf(v))
+    }
+    case None => {
+      None
+    }
+  }
+
+  def swapBranches(t: Branch[Int]): Tree[Int] = {
+    Branch(t.value, t.right, t.left)
+  }
+
+  def readInput(): (Int, Array[String], Int, Array[Int]) = {
+    val n = StdIn.readLine().toInt
+
+    val input: Array[String] = Array.ofDim(n + 1)
+    for (i <- 1 to n) {
+      input(i) = StdIn.readLine()
+    }
+    val t = StdIn.readLine().toInt
+    val ks: Array[Int] = Array.ofDim(t)
+    for (i <- 0 to t - 1) {
+      ks(i) = StdIn.readLine().toInt
+    }
+    (n, input, t, ks)
+  }
+
   def main(args: Array[String]) {
-    val input : Array[String] = "3\n2 3\n-1 -1\n-1 -1\n2\n1\n1".split("\n")
-    val n = input(0).toInt
+    val (n, input, t, ks) = readInput()
     val tree = buildNode(1, n, input)
-    inorderPrint(Option.apply(tree))
+    //    val input: Array[String] = "5\n2 3\n-1 4\n-1 5\n-1 -1\n-1 -1\n1\n2".split("\n")
+    //    val n = input(0).toInt
+    //    val t = input(n + 1)
+    //    val ks = input.splitAt(n + 2)._2.map(_.toInt)
+    // TODO swap on k*2...
+    ks.foldRight(tree)((k, t) => {
+      val newT = swap(Some(t), 1, k)
+      inorderPrint(newT)
+      println()
+      newT.get
+    })
   }
 }
